c6e27c7710c5e372dcac7811dbbcaca730ef3c2f
José Rafael Morente González <josemorenteg98@outlook.com>
2020-03-26 21:21:37 -0600

    [ADDED] Estado.cs

---
+++ OLC1-Proyecto-1/Modelo/Estado.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLC1_Proyecto_1.Modelo
+{
+    class Estado
+    {
+        private int idEstado;
+        private ArrayList transiciones = new ArrayList();
+
+        public Estado(int idEstado, ArrayList transiciones)
+        {
+            this.IdEstado = idEstado;
+            this.Transiciones = transiciones;
+        }
+
+        public Estado(int idEstado)
+        {
+            this.IdEstado = idEstado;
+        }
+
+        public Estado()
+        {
+        }
+
+        public int IdEstado { get => idEstado; set => idEstado = value; }
+        public ArrayList Transiciones { get => transiciones; set => transiciones = value; }
+
+        public void agregarTransicion(Transicion t)
+        {
+            Transiciones.Add(t);
+        }
+
+        public override string ToString()
+        {
+            return IdEstado.ToString();
+        }
+    }
+}
