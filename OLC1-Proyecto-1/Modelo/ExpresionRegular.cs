72326869525faaa3faefd4c930bb8f99e6a762fe
José Rafael Morente González <josemorenteg98@outlook.com>
2020-03-26 21:22:21 -0600

    [ADDED] ExpresionRegular.cs

---
+++ OLC1-Proyecto-1/Modelo/ExpresionRegular.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLC1_Proyecto_1.Modelo
+{
+    class ExpresionRegular
+    {
+        private String nombre;
+        private ArrayList arrayListElemento;
+
+        public ExpresionRegular()
+        {
+            ArrayListElemento = new ArrayList();
+        }
+
+        public ExpresionRegular(String nombre, ArrayList arrayListElemento)
+        {
+            this.Nombre = nombre;
+            this.ArrayListElemento = arrayListElemento;
+        }
+
+        public void toString()
+        {
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("ArrayListElemento:");
+            foreach (object item in arrayListElemento)
+            {
+                Console.WriteLine("\t-" + item);
+            }
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public ArrayList ArrayListElemento { get => arrayListElemento; set => arrayListElemento = value; }
+    }
+}
