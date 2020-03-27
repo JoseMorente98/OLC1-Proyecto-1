c55e5fd40c77f63e25586de31e6bc7a8fb9efcfe
José Rafael Morente González <josemorenteg98@outlook.com>
2020-03-26 21:23:01 -0600

    [ADDED] Transicion.cs Evaluador.cs

---
+++ OLC1-Proyecto-1/Modelo/Evaluador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLC1_Proyecto_1.Modelo
+{
+    class Evaluador
+    {
+        string nombreExpresion;
+        Automata afd;
+
+        public Evaluador(string nombreExpresion, Automata afd)
+        {
+            this.nombreExpresion = nombreExpresion;
+            this.afd = afd;
+        }
+
+        public string NombreExpresion { get => nombreExpresion; set => nombreExpresion = value; }
+        internal Automata Afd { get => afd; set => afd = value; }
+    }
+}
---
+++ OLC1-Proyecto-1/Modelo/Transicion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLC1_Proyecto_1.Modelo
+{
+    class Transicion
+    {
+        private Estado inicio;
+        private Estado fin;
+        private String simbolo;
+
+        public String Simbolo { get => simbolo; set => simbolo = value; }
+        internal Estado Inicio { get => inicio; set => inicio = value; }
+        internal Estado Fin { get => fin; set => fin = value; }
+
+        public Transicion(Estado inicio, Estado fin, String simbolo)
+        {
+            this.Inicio = inicio;
+            this.Fin = fin;
+            this.Simbolo = simbolo;
+        }
+
+        public Transicion()
+        {
+        }
+
+        public override string ToString()
+        {
+            return "(" + Inicio.IdEstado + "-" + Simbolo + "-" + Fin.IdEstado + ")";
+        }
+
+        public String DOT_String()
+        {
+
+            String text = Simbolo;
+            if (Simbolo.Contains("\\"))
+            {
+                text = text.Replace("\\", "\\\\");
+            }
+            return (Inicio + " -> " + Fin + " [label=\"" + text.Replace("\"", "") + "\"];");
+        }
+    }
+}
