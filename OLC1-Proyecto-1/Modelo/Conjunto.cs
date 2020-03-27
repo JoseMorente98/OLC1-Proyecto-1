2f5937ade7ef97c748ef0f8c3d2b22d70a7af803
José Rafael Morente González <josemorenteg98@outlook.com>
2020-03-26 21:21:26 -0600

    [ADDED] Conjunto.cs

---
+++ OLC1-Proyecto-1/Modelo/Conjunto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLC1_Proyecto_1.Modelo
+{
+    class Conjunto
+    {
+        private String nombre;
+        private ArrayList elementos;
+
+        public Conjunto()
+        {
+            ArrayListElemento = new ArrayList();
+        }
+
+        public Conjunto(String n, ArrayList ar)
+        {
+            this.Nombre = n;
+            this.ArrayListElemento = ar;
+        }
+
+        public void toString()
+        {
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("ArrayListElemento:");
+            foreach (object item in elementos)
+            {
+                Console.WriteLine("\t-" + item);
+            }
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public ArrayList ArrayListElemento { get => elementos; set => elementos = value; }
+    }
+}
