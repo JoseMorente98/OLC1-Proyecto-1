eb2d1e5e823094c74e1a062467f259b0a78b4118
José Rafael Morente González <josemorenteg98@outlook.com>
2020-03-26 21:22:06 -0600

    [ADDED] Image.cs

---
+++ OLC1-Proyecto-1/Modelo/Imagen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLC1_Proyecto_1.Modelo
+{
+    class Imagen
+    {
+        private String nombre;
+        private String tipo;
+        private String path;
+
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Path { get => path; set => path = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        public Imagen(string nombre, string tipo, string path)
+        {
+            Nombre = nombre;
+            Tipo = tipo;
+            Path = path;
+        }
+
+        public Imagen()
+        {
+        }
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
+
+    }
+}
